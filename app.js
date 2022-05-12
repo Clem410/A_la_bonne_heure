49649c38f1bd1a1be223c5874eb56cd45c712d16
Grégoire Richard <32200702+GregoireARichard@users.noreply.github.com>
2022-05-12 19:18:07 +0200

    loader done

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ function bascule_affichage(id) {
     bloc = document.getElementById(id);
     bloc.classList.toggle("visible");
 }
+const body = document.querySelector('.pageContent')
+const loader = document.querySelector('.loader')
+loader.style.display = 'block'
+body.style.display = 'none'
 
 
+window.setTimeout(()=>{
+    loader.style.display = 'none'
+    body.style.display = 'block'
+    //loader.style.display = 'none'
+},2100)
 
 
