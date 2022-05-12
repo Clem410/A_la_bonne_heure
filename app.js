8cca8c59c93cd6761ea9dc3323e95db1eebb78f2
Clem410 <63145359+Clem410@users.noreply.github.com>
2022-05-12 17:34:11 +0200

    design

--- app.js
+++ app.js
@@ -6,21 +6,3 @@ function bascule_affichage(id) {
 
 
 
-/*
-$(document).ready(function(){   
-    window.setTimeout('fadeout();', 2000);
-});
-
-function fadeout(){
-
-    $('#loader').delay(2000).fadeOut('slow', function() {
-        $('.notLoaded').removeClass('notLoaded');
-    });
-}
-
-
-window.setTimeout(()=>{
-    ('.loader').style.display = 'none'
-    ('.pageContent').style.display = 'block'
-},2100)
-*/
